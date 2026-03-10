f65730d1cf262b6d85c820a8597781d27bb3f634
Amit Singh <amit@azodha.com>
2026-03-11 01:36:57 +0530

    cursor options updated

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,19 @@
 require "nvchad.options"
 
--- add yours here!
-
+-- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Cursor styles
+vim.opt.guicursor = table.concat({
+  "n-v-c:block-blinkwait300-blinkon200-blinkoff150", -- normal/visual/command
+  "i-ci:ver25-blinkwait300-blinkon200-blinkoff150", -- insert
+  "r-cr:hor20-blinkwait300-blinkon200-blinkoff150", -- replace
+  "o:hor50", -- operator pending
+}, ",")
+
+-- Highlight current line
+vim.opt.cursorline = true
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
